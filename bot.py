6f40ca5b7554859610a4a17228127915a3b5bae6
Farhan Ashraf <farhanashrafali30@gmail.com>
2021-01-19 10:52:49 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,6 +20,9 @@ async def couldyouboostforme(ctx):
     await ctx.send("Read My Name NIGGA")
 
 @client.command()
+async def there?(ctx):
+    await ctx.send("Hazir Janab")
+@client.command()
 async def perfect(ctx):
     await ctx.send("Chal hun mera put chuti kr")
 client.run("ODAwOTUwNTg2ODUzMDMxOTY3.YAZlXA.x83RaYk0JSnF1ha4ZCuWCQp2z00") #this is where your bot's key code goes in
